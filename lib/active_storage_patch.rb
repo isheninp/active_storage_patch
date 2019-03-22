4780187f1ac2aeba7f1135b23067157639e84b9a
Pavel Ishenin <isheninp@gmail.com>
2019-03-22 11:52:48 +0300

    2203

--- lib/active_storage_patch.rb
+++ lib/active_storage_patch.rb
@@ -1,5 +1,13 @@
 require "active_storage_patch/railtie"
 
 module ActiveStoragePatch
-  
+  def show
+    expires_in 1.year, public: true
+    variant = @blob.representation(params[:variation_key]).processed
+    send_data @blob.service.download(variant.key),
+                type: @blob.content_type || DEFAULT_SEND_FILE_TYPE,
+                disposition: 'inline'
+  end
 end
+
+ActiveStorage.prepend(ActiveStoragePatch)
